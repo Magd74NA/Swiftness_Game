a5a5e394cb9137962f7e12804a0a59e04ebc18cd
Magd <{69316030}+{Magd74NA}@users.noreply.github.com>
2025-05-24 17:40:04 -0400

    removed files from last commit that are no longer relevant

--- main.swift
+++ main.swift
@@ -1,7 +1,19 @@
 import Raylib
 
-struct screenDimensions{
-  let screenName : String = "Swiftness"
+let gravity: Float32 = 2000
+let playerSpeed: Float32 = 400
+let jumpForce: Float32 = -600
+let playerScale: Float32 = 4
+let playerHeight: Float32 = 64
+
+let left: Int32 = 263
+let right: Int32 = 262
+let jump: Int32 = 32  // Space
+let jumpAlt: Int32 = 265
+let down: Int32 = 264
+
+struct screenDimensions {
+  let screenName: String = "Swiftness"
   let screenWidth: Int32 = 1280
   let screenHeight: Int32 = 720
   let targetFPS: Int32 = 144
@@ -11,7 +23,7 @@ struct screenDimensions{
     print("window start")
   }
 }
- //playing around with structs and inits probably not a good use but now I can initialize wherever I want with this one line
+//playing around with structs and inits probably not a good use but now I can initialize wherever I want with this one line
 _ = screenDimensions()
 
 //Memory ordered smallest to largest members for optimization?
@@ -20,88 +32,91 @@ struct playerProperties {
   var player_stationary = false
   var player_flip = false
 
-  var player_run_current_frame : Int = 0
+  var player_run_current_frame: Int = 0
   let player_run_num_frames = 4
-  
-  var player_run_frame_timer : Float32 = 0
-  var player_run_frame_length : Float32 = 0.1
 
-  var player_pos : Vector2 = Vector2(x : 640, y : 320)
-  var player_vel : Vector2 = Vector2(x : 0, y : 0)
-  
-  let player_run_texture : Texture = LoadTexture("cat_run.png")
+  var player_run_frame_timer: Float32 = 0
+  var player_run_frame_length: Float32 = 0.1
+
+  var player_pos: Vector2 = Vector2(x: 640, y: 320)
+  var player_vel: Vector2 = Vector2(x: 0, y: 0)
+
+  let player_run_texture: Texture = LoadTexture("cat_run.png")
 }
 
 var player = playerProperties()
+
 while !WindowShouldClose() {
 
-    BeginDrawing()
-    ClearBackground(Color(r: 110, g: 184, b: 168, a: 255))
-    player.player_vel.y += 2000 * GetFrameTime()
+  BeginDrawing()
+  ClearBackground(Color(r: 110, g: 184, b: 168, a: 255))
+  player.player_vel.y += gravity * GetFrameTime()
 
-    if IsKeyDown(263) {
-     player.player_vel.x = -400
-     player.player_flip = true
-     player.player_stationary = false
-    }else if IsKeyDown(262) {
-     player.player_vel.x = 400
-     player.player_flip = false
-     player.player_stationary = false
-    }else {
-     player.player_vel.x = 0
-     player.player_stationary = true
-    }
-   if player.player_grounded && (IsKeyPressed(32) || IsKeyPressed(265)) {
-     player.player_vel.y = -600 
-     player.player_grounded = false
-   }
-   if IsKeyPressed(264) {
-     player.player_vel.y = 800 
-   }
+  if IsKeyDown(left) {
+    player.player_vel.x = -playerSpeed
+    player.player_flip = true
+    player.player_stationary = false
+  } else if IsKeyDown(right) {
+    player.player_vel.x = playerSpeed
+    player.player_flip = false
+    player.player_stationary = false
+  } else {
+    player.player_vel.x = 0
+    player.player_stationary = true
+  }
+  if player.player_grounded && (IsKeyPressed(jump) || IsKeyPressed(jumpAlt)) {
+    player.player_vel.y = jumpForce
+    player.player_grounded = false
+  }
 
-   player.player_pos.x =  player.player_pos.x + (player.player_vel.x * GetFrameTime()) 
-   player.player_pos.y = player.player_pos.y + (player.player_vel.y * GetFrameTime())
+  player.player_pos.x = player.player_pos.x + (player.player_vel.x * GetFrameTime())
+  player.player_pos.y = player.player_pos.y + (player.player_vel.y * GetFrameTime())
 
-   if player.player_pos.y > Float32(GetScreenHeight() - 64) {
-     player.player_pos.y = Float32(GetScreenHeight() - 64)
-     player.player_grounded = true
-   }
+  if player.player_pos.y > Float32(GetScreenHeight()) - playerHeight {
 
-   let player_run_width = Float32(player.player_run_texture.width)
-   let player_run_height = Float32(player.player_run_texture.height)
+    player.player_pos.y = Float32(GetScreenHeight()) - playerHeight
+    player.player_grounded = true
+    player.player_vel.y = 0
+  }
 
-   if(!player.player_stationary){
-   player.player_run_frame_timer += GetFrameTime()
-  }else{
+  let player_run_width = Float32(player.player_run_texture.width)
+  let player_run_height = Float32(player.player_run_texture.height)
+
+  if !player.player_stationary {
+    player.player_run_frame_timer += GetFrameTime()
+  } else {
     player.player_run_current_frame = 3
-  } 
+  }
   if player.player_run_frame_timer > player.player_run_frame_length {
     player.player_run_current_frame += 1
     player.player_run_frame_timer = 0
     if player.player_run_current_frame == player.player_run_num_frames {
-        player.player_run_current_frame = 0
-        }
+      player.player_run_current_frame = 0
     }
+  }
 
-    var draw_player_source = Rectangle(
-    x: Float32(player.player_run_current_frame) * player_run_width / Float32(player.player_run_num_frames), 
-    y: 0 , 
-    width: player_run_width / Float32(player.player_run_num_frames) , 
-    height: player_run_height) 
+  var draw_player_source = Rectangle(
+    x: Float32(player.player_run_current_frame) * player_run_width
+      / Float32(player.player_run_num_frames),
+    y: 0,
+    width: player_run_width / Float32(player.player_run_num_frames),
+    height: player_run_height)
 
-   if player.player_flip {
+  if player.player_flip {
     draw_player_source.width = -draw_player_source.width
-   }
+  }
 
-   let draw_player_dest = Rectangle (
-      x : player.player_pos.x,
-      y : player.player_pos.y,
-      width : player_run_width * 4 / Float32(player.player_run_num_frames),
-      height : player_run_height * 4
+  let draw_player_dest = Rectangle(
+    x: player.player_pos.x,
+    y: player.player_pos.y,
+    width: player_run_width * playerScale / Float32(player.player_run_num_frames),
+    height: player_run_height * playerScale
   )
 
-   DrawTexturePro(player.player_run_texture, draw_player_source, draw_player_dest, Vector2(x:0,y:0), 0, Color(r: 245, g: 245, b: 245, a: 255))
-   EndDrawing()
-  }
-  CloseWindow()
-
+  DrawTexturePro(
+    player.player_run_texture, draw_player_source, draw_player_dest, Vector2(x: 0, y: 0), 0,
+    Color(r: 245, g: 245, b: 245, a: 255))
+  EndDrawing()
+}
+UnloadTexture(player.player_run_texture) 
+CloseWindow()
